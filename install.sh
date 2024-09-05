fb30f7a18ebd99f10d5b6dae5ebc6937bd585786
Jeff T. Polczynski <jefftp@jefftp.com>
2024-09-05 17:10:03 -0500

    Adding script to help with install.

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Run Disko to partition disks
+nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./disko/btrfs.nix
+
+# Generate initial configs
+nixos-generate-config --no-filesystems --root /mnt
