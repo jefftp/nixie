95927944a47d94d65004f2e3c2cb5bfed51b6e6b
Jeff T. Polczynski <jefftp@jefftp.com>
2024-09-05 13:36:46 -0500

    Rename disko configuration. Remove swap partition. Add disk argument to ease configuration.

---
+++ disko/btrfs.nix
@@ -0,0 +1,74 @@
+# Partition Layout
+#
+# /boot - FAT32 - 1G
+# volume - BTRFS - Rest of Disk
+#   /root - Subvol - /
+#   /home - Subvol - /home
+#   /nix - Subvol - /nix
+#
+{
+  lib,
+  pkgs,
+  disk ? "/dev/sda",
+  configVars,
+  ...
+}:
+{
+  disko.devices = {
+    disk = {
+      disk0 = {
+        type = "disk";
+        device = disk;
+        content = {
+          type = "gpt";
+          partitions = {
+            # Set up the EFI Startup Partition
+            ESP = {
+              type = "EF00";
+              size = "1G";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "defaults" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ]; # Force overwriting any existing partition
+                subvolumes = {
+                  # Root subvolume
+                  "@root" = {
+                    mountpoint = "/";
+                    mountOptions = [
+                      "compress-force=zstd:1"
+                      "noatime"
+                    ];
+                  };
+                  # Home subvolume
+                  "@home" = {
+                    mountpoint = "/home";
+                    mountOptions = [
+                      "compress-force=zstd:1"
+                      "noatime"
+                    ];
+                  };
+                  # Nix subvolume
+                  "@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [
+                      "compress-force=zstd:1"
+                      "noatime"
+                    ];
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
